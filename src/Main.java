4b8d95a29c4af8ec4687fc3de1e169c364fa720f
Mikael Edvardsson <Mikael.Edvardsson@elev.ga.ntig.se>
2024-10-14 08:47:42 +0200

    first

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter your tamagotchi name: ");
+
+        Tamagotchi tamagotchi = new Tamagotchi(name);
+
+
+
+    }
+}
