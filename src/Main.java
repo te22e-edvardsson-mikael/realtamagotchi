413366fb6eb3d4fdfa2f5c53d7aa416980412ea5
Mikael Edvardsson <Mikael.Edvardsson@elev.ga.ntig.se>
2024-10-14 09:04:40 +0200

    first

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,27 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("enter your tamagotchi name: ");
+        String name = scanner.nextLine();
 
         Tamagotchi tamagotchi = new Tamagotchi(name);
+
+        while (tamagotchi.getAlive()){
+            System.out.println("what would you like to do?");
+            System.out.println("1. Feed your tamagotchi");
+            System.out.println("2. Teach your tamagotchi");
+            System.out.println("3. print your tamagotchi's stats");
+            System.out.println("4. Exit");
+
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (choice) {
+                case 1:
+                    tamagotchi.feed();
+            }
+        }
+
+
 
 
 
--- src/Tamagotchi.java
+++ src/Tamagotchi.java
@@ -1,2 +1,57 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Tamagotchi {
+    private int hunger;
+    private int boredom;
+    private List<String> words;
+    private boolean isAlive;
+    public String name;
+
+    public Tamagotchi(String name) {
+        this.name = name;
+        hunger = 0;
+        boredom = 0;
+        words = new ArrayList<>();
+        isAlive = true;
+    }
+
+    public void feed(){
+        hunger--;
+        hunger = Math.max(0, hunger);
+        System.out.println(name+" is less hungry");
+    }
+
+  public int teach(String word) {
+        words.add(word);
+      System.out.println(name+" has learned the word "+word);
+      reduceBoredom();
+  }
+
+  public void tick() {
+        hunger++;
+        boredom++;
+        reduceBoredom();
+        if (hunger >= 10 || boredom >= 10) {
+            isAlive = false;
+        }
+  }
+
+  public void printstats(){
+      System.out.println("name: "+name);
+      System.out.println("hunger: "+hunger);
+      System.out.println("words: "+words);
+      System.out.println("isAlive: "+isAlive);
+  }
+
+  public boolean getAlive(){
+        return isAlive;
+  }
+
+  private void reduceBoredom() {
+        int boredomReduction = words.size()/2;
+        boredom -= boredomReduction;
+        boredom = Math.max(0, boredom);
+  }
+
 }
